f6e71565fee07d2c8e2e5f3a7066b216ea6c506f
Kai Yamada <gpioblink@aol.com>
2019-04-29 09:30:21 +0900

    remove code for incoming

--- heisei_quiz.ino
+++ heisei_quiz.ino
@@ -12,38 +12,12 @@ WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
 const char* topic = "test";     // 送信先のトピック名
-const char* subtopic = "test";  //受信するトピック名
 char* payload;                   // 送信するデータ
-
-const int LED_RED = 26;
-const int LED_BLUE = 27;
-const int LED_YELLOW = 14;
-const int LED_GREEN = 12;
 
 const int SW_RED = 34;
 const int SW_BLUE = 35;
 const int SW_YELLOW = 32;
 const int SW_GREEN = 33;
-volatile int incomingByte = 0;
-volatile int previousByte = 0;
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
-  }
-  if((char)payload[0] == '0'){
-    updateStatus(LED_RED);
-  } else if((char)payload[0] == '1'){
-    updateStatus(LED_BLUE);
-  } else if((char)payload[0] == '2'){
-    updateStatus(LED_YELLOW);
-  } else if((char)payload[0] == '3'){
-    updateStatus(LED_GREEN);
-  }
-  Serial.println();
-}
 
 void setup() {
     Serial.begin(38400);
@@ -54,10 +28,6 @@ void setup() {
     // Connect MQTT
     connectMqtt();
 
-    pinMode(LED_RED, OUTPUT);
-    pinMode(LED_BLUE, OUTPUT);
-    pinMode(LED_YELLOW, OUTPUT);
-    pinMode(LED_GREEN, OUTPUT);
     pinMode(SW_RED, INPUT);
     pinMode(SW_BLUE, INPUT);
     pinMode(SW_YELLOW, INPUT);
@@ -153,15 +123,3 @@ void connectMqtt()
         randomSeed(micros());
     }
 }
-/**
- * Change LED Color
- */
-void updateStatus(int colorid)
-{
-  Serial.print("TURN ON ");
-  Serial.println(colorid);
-  digitalWrite(LED_RED,LOW);
-  digitalWrite(LED_BLUE,LOW);
-  digitalWrite(LED_YELLOW,LOW);
-  digitalWrite(LED_GREEN,LOW);
-  digitalWrite(colorid,HIGH);
